ab74d3de52138ad225ba6451bbf7614847fca42e
DiegoSouzaOni <diego.souza.oni@gmail.com>
2024-09-02 12:26:02 +0000

    Added `.database.js` and  `.env`files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1+ 1 as Sum;");
+  console.log(result.rows);
+
   response.status(200).json({ chave: "São acima da média" });
 }
 
