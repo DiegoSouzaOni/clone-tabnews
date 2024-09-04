f6644c05e4b8c98b820ecb8c7aa5b3c13d19bc24
DiegoSouzaOni <diego.souza.oni@gmail.com>
2024-09-04 14:22:32 +0000

    Added a more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -1,5 +1,4 @@
 import { Client } from "pg";
-// const client = new Client;
 
 async function query(queryObject) {
   const client = new Client({
@@ -8,7 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValues(),
   });
 
   try {
@@ -26,3 +25,12 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === "development" ? false : true;
+}
