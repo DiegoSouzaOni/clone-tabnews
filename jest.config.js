e7756d52819a3f8cb3708fd6e5c0d0be8c7c805d
DiegoSouzaOni <diego.souza.oni@gmail.com>
2024-09-16 18:33:44 -0300

    Revert "Makes `npm test` more robust with `async-retry` and `orchestrator.js`"

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,6 @@ const createJestConfig = nextJest({
 });
 const jestConfig = createJestConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
-  testTimeout: 60000,
 });
 
 module.exports = jestConfig;
--- tests/migrations/post.test.js
+++ tests/migrations/post.test.js
@@ -1,10 +1,10 @@
 import database from "infra/database.js";
-import orchestrator from "tests/orchestrator";
 
-beforeAll(async () => {
-  await orchestrator.waitForAllServices();
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
   await database.query("drop schema public cascade;create schema public;");
-});
+}
 
 test("POST to /api/v1/migrations should return 201", async () => {
   const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
