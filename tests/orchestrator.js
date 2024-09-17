f1a1bf9abc14201d8afef758e77a50ce27cc49e0
DiegoSouzaOni <diego.souza.oni@gmail.com>
2024-09-17 01:38:07 +0000

    Fixed `prettier` style linting

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -11,7 +11,7 @@ async function waitForAllServices() {
 
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
-      
+
       if (response.status !== 200) {
         throw Error();
       }
