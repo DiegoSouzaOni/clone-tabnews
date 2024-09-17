588828629f2d5173537829cdd60891e3ffdfbcdc
DiegoSouzaOni <diego.souza.oni@gmail.com>
2024-09-17 02:09:58 +0000

    Fixed `eslint` linting

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,5 +1,4 @@
 const { exec } = require("node:child_process");
-const { log } = require("node:console");
 
 function checkPostgres() {
   exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
